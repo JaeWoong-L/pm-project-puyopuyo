883aa530ade68160b30cecd41fb204b5f49d50e3
JaeWoong.Lee <59162713+JaeWoong-L@users.noreply.github.com>
2021-11-21 02:00:41 +0900

    add Vector3f & Vector4f files

---
+++ FinalProject/Vector3f.cpp
@@ -0,0 +1,49 @@
+#include "Vector3f.h"
+
+Vector3f::Vector3f() {
+	pos[0] = pos[1] = pos[2] = 0.0f;
+}
+
+Vector3f::Vector3f(float x, float y, float z) {
+	pos[0] = x;
+	pos[1] = y;
+	pos[2] = z;
+}
+
+void Vector3f::setPos(float x, float y, float z) {
+	pos[0] = x;
+	pos[1] = y;
+	pos[2] = z;
+}
+
+float& Vector3f::operator[](const int i) {
+	return pos[i];
+}
+
+float Vector3f::operator[](const int i) const {
+	return pos[i];
+}
+
+Vector3f& Vector3f::operator+=(const Vector3f& v) {
+	pos[0] += v.pos[0];
+	pos[1] += v.pos[1];
+	pos[2] += v.pos[2];
+	
+	return (*this);
+}
+
+Vector3f Vector3f::operator-(const Vector3f& v) {
+	return Vector3f(pos[0] - v.pos[0], pos[1] - v.pos[1], pos[2] - v.pos[2]);
+}
+
+Vector3f Vector3f::operator*(const float s) {
+	return Vector3f(s * pos[0], s * pos[1], s * pos[2]);
+}
+
+float dotProduct(Vector3f v1, Vector3f v2) {
+	float ans = 0.0f;
+	for (int i = 0; i < 3; i++)
+		ans += v1[i] * v2[i];
+
+	return ans;
+}
---
+++ FinalProject/Vector3f.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class Vector3f {
+public:
+	Vector3f();
+	Vector3f(float x, float y, float z);
+	void setPos(float x, float y, float z);
+	float& operator[](const int i);
+	float operator[](const int i) const;
+	Vector3f& operator+=(const Vector3f& v);
+	Vector3f operator-(const Vector3f& v);
+	Vector3f operator*(const float s);
+
+private:
+	float pos[3];
+};
+
+float dotProduct(Vector3f v1, Vector3f v2);
