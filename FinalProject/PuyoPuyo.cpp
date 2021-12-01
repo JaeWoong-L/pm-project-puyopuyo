1ba8dcc2a7c191fff5d94fe540abf81af1986c21
JaeWoong.Lee <59162713+JaeWoong-L@users.noreply.github.com>
2021-12-01 11:42:10 +0900

    멤버 변수 데이터타입 변경 및 추가
    1. puyo1, puyo2 를 배열로 통합
    2. enum class relation 추가

--- FinalProject/PuyoPuyo.cpp
+++ FinalProject/PuyoPuyo.cpp
@@ -1,51 +1,55 @@
 #include "PuyoPuyo.h"
 
-PuyoPuyo::PuyoPuyo() : puyo1(), puyo2(), collisionDetected(false) {}
+PuyoPuyo::PuyoPuyo() : puyo{}, collisionDetected(false), relation(Relation::LEFT) {}
 
-PuyoPuyo::PuyoPuyo(const Puyo& sph1, const Puyo& sph2) : collisionDetected(false) {
-	puyo1 = sph1;
-	puyo2 = sph2;
+PuyoPuyo::PuyoPuyo(const Puyo& sph1, const Puyo& sph2) : collisionDetected(false), relation(Relation::LEFT) {
+	puyo[0] = sph1;
+	puyo[1] = sph2;
 }
 
 void PuyoPuyo::setPuyos(const Puyo& sph1, const Puyo& sph2) {
-	puyo1 = sph1;
-	puyo2 = sph2;
+	puyo[0] = sph1;
+	puyo[1] = sph2;
 }
 
-Puyo& PuyoPuyo::getFirstPuyo() {
-	return puyo1;
+void PuyoPuyo::setRelation(Relation r) {
+	relation = r;
 }
 
-Puyo& PuyoPuyo::getSecondPuyo() {
-	return puyo2;
-}
+//Relation PuyoPuyo::getRelation() const {
+	//if (puyo[0].getCenter()[0] == puyo[1].getCenter()[0]) {
+	//	if (puyo[0].getCenter()[1] > puyo[1].getCenter()[1])
+	//		return UP;
+	//	else
+	//		return DOWN;
+	//}
+	//else {
+	//	if (puyo[0].getCenter()[0] < puyo[1].getCenter()[0])
+	//		return LEFT;
+	//	else
+	//		return RIGHT;
+	//}
+//}
 
-const int PuyoPuyo::getRelation() const {
-	if (puyo1.getCenter()[0] == puyo2.getCenter()[0]) {
-		if (puyo1.getCenter()[1] > puyo2.getCenter()[1])
-			return UP;
-		else
-			return DOWN;
-	}
-	else {
-		if (puyo1.getCenter()[0] < puyo2.getCenter()[0])
-			return LEFT;
-		else
-			return RIGHT;
-	}
+PuyoPuyo::Relation PuyoPuyo::getRelation() const {
+	return relation;
 }
 
 void PuyoPuyo::move() {
-	puyo1.move();
-	puyo2.move();
+	puyo[0].move();
+	puyo[1].move();
 }
 
 void PuyoPuyo::manualMove(int key, int pixelSize) {
-	puyo1.manualMove(key, pixelSize);
-	puyo2.manualMove(key, pixelSize);
+	puyo[0].manualMove(key, pixelSize);
+	puyo[1].manualMove(key, pixelSize);
 }
 
 void PuyoPuyo::draw() const {
-	puyo1.draw();
-	puyo2.draw();
+	puyo[0].draw();
+	puyo[1].draw();
+}
+
+Puyo& PuyoPuyo::operator[](int i) {
+	return puyo[i];
 }
--- FinalProject/PuyoPuyo.h
+++ FinalProject/PuyoPuyo.h
@@ -4,7 +4,7 @@
 
 class PuyoPuyo : public Puyo {
 public:
-	enum relation {
+	enum class Relation {
 		UP,
 		DOWN,
 		LEFT,
@@ -14,15 +14,17 @@ public:
 	PuyoPuyo();
 	PuyoPuyo(const Puyo& sph1, const Puyo& sph2);
 	void setPuyos(const Puyo& sph1, const Puyo& sph2);
-	Puyo& getFirstPuyo();
-	Puyo& getSecondPuyo();
-	const int getRelation() const;
+	void setRelation(Relation r);
+	Relation getRelation() const;
 
 	void move();
 	void manualMove(int key, int pixelSize);
 	void draw() const;
 
+	Puyo& operator[] (int i);
+
 private:
-	Puyo puyo1, puyo2;
+	Puyo puyo[2];
 	bool collisionDetected;
+	Relation relation;
 };
